8b30a0abfb0d07611017ae747d905890a62635f8
IAGO <iago.mendonca_2005@hotmail.com>
2024-02-05 17:56:40 -0300

    feat: create transition and calculadora_imc

--- portfolio_iago/app_python/models.py
+++ portfolio_iago/app_python/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+from static import pesoUsuario,alturaUsuario
+import requests
+url = "https://body-mass-index-bmi-calculator.p.rapidapi.com/metric"
+querystring = {pesoUsuario,alturaUsuario}
+
+headers = {
+	"X-RapidAPI-Key": "f5d22b74a5msh72e7f64685d971cp1c73d5jsn48e98adb797f",
+	"X-RapidAPI-Host": "body-mass-index-bmi-calculator.p.rapidapi.com"
+}
+
+response = requests.get(url, headers=headers, params=querystring)
+
+
